577bf976f9f6a01dfbb2cc94ec922a436742beda
XcenaX <40425017+XcenaX@users.noreply.github.com>
2019-03-21 12:00:43 +0600

    Add files via upload

---
+++ WindowsFormsApplication13/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApplication13 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+                        
+            UpdateUI = new Action<int>(delegate(int value) {
+                progressBar1.Value = value;
+            });            
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CopyFileEx(string lpExistingFileName, string lpNewFileName,
+           CopyProgressRoutine lpProgressRoutine, IntPtr lpData, ref Int32 pbCancel,
+           CopyFileFlags dwCopyFlags);
+
+        delegate CopyProgressResult CopyProgressRoutine(
+        long TotalFileSize,
+        long TotalBytesTransferred,
+        long StreamSize,
+        long StreamBytesTransferred,
+        uint dwStreamNumber,
+        CopyProgressCallbackReason dwCallbackReason,
+        IntPtr hSourceFile,
+        IntPtr hDestinationFile,
+        IntPtr lpData);
+
+        int pbCancel;
+
+        enum CopyProgressResult : uint {
+            PROGRESS_CONTINUE = 0,
+            PROGRESS_CANCEL = 1,
+            PROGRESS_STOP = 2,
+            PROGRESS_QUIET = 3
+        }
+
+        enum CopyProgressCallbackReason : uint {
+            CALLBACK_CHUNK_FINISHED = 0x00000000,
+            CALLBACK_STREAM_SWITCH = 0x00000001
+        }
+
+        [Flags]
+        enum CopyFileFlags : uint {
+            COPY_FILE_FAIL_IF_EXISTS = 0x00000001,
+            COPY_FILE_RESTARTABLE = 0x00000002,
+            COPY_FILE_OPEN_SOURCE_FOR_WRITE = 0x00000004,
+            COPY_FILE_ALLOW_DECRYPTED_DESTINATION = 0x00000008
+        }
+
+        private void XCopy(string oldFile, string newFile) {
+            CopyFileEx(oldFile, newFile, new CopyProgressRoutine(this.CopyProgressHandler), IntPtr.Zero, ref pbCancel, CopyFileFlags.COPY_FILE_RESTARTABLE);
+        }
+
+        private CopyProgressResult CopyProgressHandler(long total, long transferred, long streamSize, long StreamByteTrans, uint dwStreamNumber, CopyProgressCallbackReason reason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData) {
+                        
+            int currentProgress = (int)((transferred/(float)total)*100);
+
+            if (this.InvokeRequired)
+                this.Invoke(UpdateUI, currentProgress);
+            else
+                UpdateUI(currentProgress);
+          
+
+            return CopyProgressResult.PROGRESS_CONTINUE;
+        } 
+        
+        private void button2_Click(object sender, EventArgs e) {            
+            ParameterizedThreadStart st = new ParameterizedThreadStart(StartCoping);
+            Thread thread = new Thread(st);
+            thread.IsBackground = true;
+            thread.Start();            
+        }
+
+        private void StartCoping(object parameters)
+        {
+            string oldFileName = textBox1.Text;
+            string newFileName = textBox2.Text;
+            XCopy(oldFileName, newFileName);            
+        }
+
+        
+        private delegate void Action<T>(T value);
+
+        
+        private Action<int> UpdateUI;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = dialog.FileName;
+            }
+        }
+    }
+}
